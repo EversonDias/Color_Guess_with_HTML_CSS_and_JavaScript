52421749f199e02b3f1f1684fd22e4d99b8de061
everson dias <dev@DESKTOP-229V1QG.localdomain>
2022-10-10 13:03:01 -0300

    added bolls color, label Of color selected and teg  p escolha uma cor

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 const containerBall = document.getElementById('containerBall');
+const balls = document.getElementsByClassName('ball');
+const labelColor = document.getElementById('rgb-color')
+
+
+function random(props=255) {
+  const number = Math.floor(Math.random() * props)
+  return number
+}
+
+function colorGenerator() { 
+  const color = `rgb(${random()}, ${random()}, ${random()})`;
+  return color;
+}
+
+function fillDivBall() {
+  for (const ball of balls) {
+    // ball.setAttribute('style', `background: ${colorGenerator()}`)
+    ball.style.backgroundColor = colorGenerator()
+  }
+
+}
+
+function getColorDivBackground() {
+  console.log('ok');
+}
+
+for (let index = 0; index < 6; index += 1) {
+  const divBall = document.createElement('div');
+  divBall.classList.add('ball');
+  divBall.addEventListener('click', getColorDivBackground);
+  containerBall.appendChild(divBall)
+  fillDivBall()
+}
+
+const colorSelect = balls[random(6)].style.backgroundColor;
+labelColor.innerHTML = colorSelect.replace('rgb', '')
+
+
