b862303232488c3cdcaf49280571784e30df811b
everson dias <dev@DESKTOP-229V1QG.localdomain>
2022-10-10 15:30:40 -0300

    fixed ESLint

--- script.js
+++ script.js
@@ -2,47 +2,57 @@ const containerBall = document.getElementById('containerBall');
 const balls = document.getElementsByClassName('ball');
 const labelColor = document.getElementById('rgb-color');
 const labelIndication = document.getElementById('answer');
+const buttonReset = document.getElementById('reset-game');
+const labelCount = document.getElementById('score');
+let count = 0;
 
-
-function random(props=255) {
-  const number = Math.floor(Math.random() * props)
-  return number
-}
-
-function colorGenerator() { 
-  const color = `rgb(${random()}, ${random()}, ${random()})`;
-  return color;
+function random(props = 255) {
+  const number = Math.floor(Math.random() * props);
+  return number;
 }
 
 function fillDivBall() {
   for (const ball of balls) {
-    ball.style.backgroundColor = colorGenerator()
+    ball.style.backgroundColor = `rgb(${random()}, ${random()}, ${random()})`;
   }
+}
 
+function selectorOfColor() {
+  const colorSelect = balls[random(6)].style.backgroundColor;
+  labelColor.innerHTML = colorSelect.replace('rgb', '');
+  localStorage.setItem('color', colorSelect);
 }
 
 function colorComparator(myColor) {
-  if (myColor === colorSelect) {
-    labelIndication.innerHTML = 'Acertou!'
+  console.log(localStorage.getItem('color'));
+  if (myColor === localStorage.getItem('color')) {
+    labelIndication.innerHTML = 'Acertou!';
+    fillDivBall();
+    selectorOfColor();
+    count += 3;
+    labelCount.innerHTML = count;
   } else {
-    labelIndication.innerHTML = 'Errou! Tente novamente!'
+    labelIndication.innerHTML = 'Errou! Tente novamente!';
   }
 }
 
 function getColorDivBackground(event) {
   const myColor = event.target.style.backgroundColor;
-  colorComparator(myColor)
+  colorComparator(myColor);
 }
 
 for (let index = 0; index < 6; index += 1) {
   const divBall = document.createElement('div');
   divBall.classList.add('ball');
   divBall.addEventListener('click', getColorDivBackground);
-  containerBall.appendChild(divBall)
-  fillDivBall()
+  containerBall.appendChild(divBall);
+  fillDivBall();
 }
 
-const colorSelect = balls[random(6)].style.backgroundColor;
-labelColor.innerHTML = colorSelect.replace('rgb', '')
+selectorOfColor();
 
-
+buttonReset.addEventListener('click', () => {
+  fillDivBall();
+  selectorOfColor();
+  labelIndication.innerHTML = 'Escolha uma cor';
+});
